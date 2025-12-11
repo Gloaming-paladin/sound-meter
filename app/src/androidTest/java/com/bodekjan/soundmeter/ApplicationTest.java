cd89c94c91421e3f1108b7023ccc3dbe7059da74
Gloaming-paladin <3020832067@qq.com>
2025-12-11 14:02:10 +0800

    version1.0

--- app/src/androidTest/java/com/bodekjan/soundmeter/ApplicationTest.java
+++ app/src/androidTest/java/com/bodekjan/soundmeter/ApplicationTest.java
@@ -1,13 +1,42 @@
 package com.bodekjan.soundmeter;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
 
 /**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ * Instrumented test, which will execute on an Android device.
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+
+    @Test
+    public void testApplicationContext() {
+        // Context of the app under test
+        Context context = ApplicationProvider.getApplicationContext();
+        assertNotNull(context);
+        assertTrue(context instanceof Application);
+    }
+
+    @Test
+    public void testPackageName() {
+        // Test that the app's package name is correct
+        Context appContext = ApplicationProvider.getApplicationContext();
+        assertEquals("com.bodekjan.soundmeter", appContext.getPackageName());
+    }
+
+    @Test
+    public void testInstrumentationRegistry() {
+        // Test instrumentation registry context
+        Context instrumentationContext = InstrumentationRegistry.getInstrumentation().getContext();
+        assertNotNull(instrumentationContext);
     }
 }
